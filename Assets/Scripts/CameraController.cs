b54ffd49e6ab667852f0b4250fd3aa0610dc523c
Pchamarra <124417418+Pchamarra@users.noreply.github.com>
2025-01-25 17:47:07 -0300

    Cambios (sobre todo anim)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -11,14 +12,33 @@ public class CameraController : MonoBehaviour
 
     Vector3 _offset;
 
+    public float distance = 10.0f;
+    private float currentX = 50.0f;
+    private float currentY = -90.0f;
+    public float sensivity = 150.0f;
+
     private void Start()
     {
         _offset = transform.position + Vector3.forward * _zOffset;
         if (!_target) _target = FindObjectOfType<Hamster>().transform;
     }
 
-    private void LateUpdate()
+    //private void LateUpdate()
+    //{
+    //    transform.position = _target.position + _offset;
+    //}
+
+    void LateUpdate()
     {
-        transform.position = _target.position + _offset;
+
+        currentY += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+
+        Vector3 Direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentX, currentY, 0);
+        transform.position = _target.position + rotation * Direction;
+
+        transform.LookAt(_target.position);
+
+
     }
 }
--- Assets/Scripts/Hamster.cs
+++ Assets/Scripts/Hamster.cs
@@ -9,10 +9,16 @@ public class Hamster : MonoBehaviour
     [SerializeField] float _speed = .25f;
     [SerializeField] float _fallDamage = 5f;
     [SerializeField] float _falYThreshold = -5f;
+    [SerializeField] float _rotSpeed;
     [Header("References")]
     [SerializeField] Rigidbody _rb;
+    [SerializeField] GameObject _hamsterMesh;
+    [SerializeField] Animator _anim;
 
     Vector3 _checkpoint;
+    Vector3 _lastValidDir;
+
+    float _xAxis, _zAxis;
 
     void Start()
     {
@@ -23,7 +29,9 @@ public class Hamster : MonoBehaviour
 
     private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.1)) TakeDamage(1f);
+        _xAxis = Input.GetAxis("Horizontal");
+        _zAxis = Input.GetAxis("Vertical");
+
 #if UNITY_EDITOR
         //Cheatcodes, solo funcionan en el editor
         if (Input.GetKeyDown(KeyCode.Return)) UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
@@ -35,12 +43,25 @@ public class Hamster : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        _anim.SetBool("Running", (_zAxis != 0 || _xAxis != 0));
+        Debug.Log($"Running: {(_zAxis != 0 || _xAxis != 0)}");
         Movement();
+        MeshRotation();
+    }
+
+    void MeshRotation()
+    {
+        //Vector3 Dir = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized; //Creo vector dirección determinado por los WASD que estoy apretando
+        Vector3 Dir = new Vector3(-_zAxis, 0f, _xAxis).normalized; //Creo vector dirección determinado por los WASD que estoy apretando
+
+        if (Dir != Vector3.zero) _lastValidDir = Dir;
+        //if (Dir != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Dir), Time.fixedDeltaTime * _rotSpeed); //anterior
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_lastValidDir), Time.fixedDeltaTime * _rotSpeed);
     }
 
     void Movement()
     {
-        Vector3 dir = new Vector3(-Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal")).normalized;
+        Vector3 dir = new Vector3(-_zAxis, 0f, _xAxis).normalized;
         _rb.AddForce(dir * _speed);
 
         if (transform.position.y <= _falYThreshold) PlayerFellOutOfMap();
@@ -54,9 +75,17 @@ public class Hamster : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        LevelManager.Instance.AnxietyTimer -= damage;
         Debug.Log($"el hamster perdió {damage} de sanidad! Nueva sanidad {LevelManager.Instance.AnxietyTimer}");
         //if (LevelManager.Instance.AnxietyTimer <= 0) LevelManager.Instance.GameOver();
+
+        if (LevelManager.Instance.AnxietyTimer > 0)
+        {
+            LevelManager.Instance.AnxietyTimer -= damage;
+        }
+        else
+        {
+            //LevelManager.Instance.currentExtraTimerBeforeKill -= damage;
+        }
     }
 
     public void Heal(float amount)

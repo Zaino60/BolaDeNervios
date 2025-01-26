9394e1506de58620f5029f96ca5ad406ee7f75b1
Martu151 <martuselem@gmail.com>
2025-01-26 14:27:27 -0300

    Spring Bouncy

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -7,12 +7,33 @@ public class Spring : MonoBehaviour
     [Header("Values")]
     [SerializeField] float _jumpForce;
 
+    AudioSource AS;
+    [SerializeField] AudioClip[] sounds;
+
+    Animator anim;
+
+    private void Start()
+    {
+        AS = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Hamster>())
         {
             Debug.Log("boing");
             other.GetComponent<Rigidbody>().AddForce(transform.up * _jumpForce);
+
+            int randomNum = Random.Range(0, sounds.Length);
+            AS.clip = sounds[randomNum];
+            if (AS.isPlaying)
+            {
+                AS.Stop();
+            }
+            AS.Play();
+
+            anim.Play(Animator.StringToHash("SpringBounce"));
         }
     }
 }
